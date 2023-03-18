c16670082445c04cdcea8f0c4998ca28729a4bd3
Abhishek Silawat <absilawat@gmail.com>
2023-03-18 23:10:52 +0530

    updated icons tech stacks

---
+++ js/icons.js
@@ -0,0 +1,46 @@
+export const frontSkillsList = [
+  {
+    label: "HTML5",
+    src: "https://img.icons8.com/color/480/000000/html-5--v1.png",
+  },
+  {
+    label: "CSS3",
+    src: "https://img.icons8.com/color/480/000000/css3.png",
+  },
+  {
+    label: "Javascript",
+    src: "https://img.icons8.com/color/480/000000/javascript.png",
+  },
+  {
+    label: "React",
+    src: "https://img.icons8.com/color/480/000000/react-native.png",
+  },
+  {
+    label: "Redux",
+    src: "https://img.icons8.com/color/480/000000/redux.png",
+  },
+
+  {
+    label: "Github",
+    src: "https://img.icons8.com/fluent/480/000000/github.png",
+  },
+  {
+    label: "NPM",
+    src: "https://img.icons8.com/color/480/000000/npm.png",
+  },
+];
+
+export const backSkillsList = [
+  {
+    label: "Node JS",
+    src: "https://img.icons8.com/color/480/000000/nodejs.png",
+  },
+  {
+    label: "Express JS",
+    src: "https://i.ibb.co/9smDM6j/pngegg.png",
+  },
+  {
+    label: "Mongo dB",
+    src: "https://i.ibb.co/ZVh01dm/rsz-mong2.png",
+  },
+];
